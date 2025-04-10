a576585d48daadaa7f77ab3ac724c67af7b56ec7
azusagawasakt <161151649+azusagawasakt@users.noreply.github.com>
2025-04-10 07:44:31 +0500

    Update views.py

--- Mydiary/views.py
+++ Mydiary/views.py
@@ -3,7 +3,7 @@ from .models import memory
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Create your views here
 @login_required(login_url='/accounts/login')
 def show(request):
     log_user = request.user
